c0636cf62a9cd8e0696158fd75f76baa0ac58c77
wlzhou <zhongbingwang@yeah.net>
2022-01-02 19:57:46 +0800

    new collections from nets

---
+++ awesomeProject/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+"fmt"
+)
+
+func main() {
+	// var a int
+	// fmt.Scan(&a)
+	fmt.Printf("%s", "hello world")
+	nums:=[]int{1,3,5,2,4,6,8,0}
+	res:=longestIncreasingSubArray(nums)
+	fmt.Println(res)
+
+}
+func longestIncreasingSubArray(nums []int) []int{
+	if len(nums)==0 {
+		return []int{}
+	}
+	if len(nums)==1 {
+		return nums
+	}
+	res :=[]int{}
+	for i:=0;i<len(nums)-1;i++ {
+		temp:=[]int{nums[i]}
+		j:=i+1
+		for ;j<len(nums);j++ {
+			if nums[j]>nums[i]{
+				temp = append(temp, nums[j])
+			}else {
+				break
+			}
+		}
+		i=j
+		if len(res)<len(temp) {
+			res=append([]int{},temp...)
+		}
+	}
+	return res
+}
